ba5847eac4ecc1cac448f22f4937d175e394fdfd
mykh1 <neige391124@yahoo.co.jp>
2021-09-04 20:43:07 +0900

    createアクションのrender変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,14 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.new(comment_params)
-    if comment.save
+    @comment = Comment.new(comment_params)
+    if @comment.save
       redirect_to prototype_path(comment.prototype)
      else
-      prototype = comment.prototype
-      comments = prototype.comments
-      render prototypes/show
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+
+      render "prototypes/show"
      end
   end
 
